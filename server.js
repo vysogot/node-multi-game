8a3ab66ea5aea3a4ddf2d9160d05204ee12a32c2
Jakub Godawa <jakub.godawa@xing.com>
2011-04-27 16:44:16 +0200

    css on, sass off - temporarly

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.configure(function(){
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.compiler({ src: __dirname + '/public', enable: ['sass'] }));
+    // app.use(express.compiler({ src: __dirname + '/public', enable: ['sass'] }));
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
 });
